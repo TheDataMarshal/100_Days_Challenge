dbf3248d6f92bb431c44a2fc8cddec0927ada28c
TheDataMarshal <thedatamarshal@gmail.com>
2021-02-12 17:30:19 +0530

    Day 7 of 100. Added DB, created User, Posts and also connected Admin Panel to Post model.

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,24 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-	{
-		'author': 'The Data Marshal',
-		'title': 'Blog post 1',
-		'content': 'First post content',
-		'date_posted': 'February 10, 2021'
-	},
-	{
-		'author': 'Some other Author',
-		'title': 'Blog post 2',
-		'content': 'Second post content',
-		'date_posted': 'February 01, 2021'
-	}
 
-]
 
 def home(request):
 	context = {
-		'posts' : posts
+		'posts' : Post.objects.all()
 	}
 	return render(request, 'blog/home.html', context)
 
